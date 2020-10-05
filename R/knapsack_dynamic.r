5dbaecf301fef97cbd6b7d18d0d7a1580c9a19d1
Rojan Karakaya <rojankarakaya@gmail.com>
2020-10-05 14:48:05 +0200

    The knapsack_dynamic function now returns the combination as well; also, better stopifnot-conditions

--- R/knapsack_dynamic.r
+++ R/knapsack_dynamic.r
@@ -1,29 +1,58 @@
 knapsack_dynamic <- function(x, W){
-  stopifnot((length(x$v)==length(x$w)))
+  stopifnot(inherits(x,"data.frame"))
+  stopifnot(W>0)
 
 
-
+  x$origIndex=1:nrow(x)
   combinations=matrix(data=FALSE,nrow=length(x$v),ncol=W)
+
+  lastRow<-rep(list(),W)
+  thisRow<-rep(list(),W)
 
   x=x[order(x$w),]
 
   for(i in 1:length(x$v)){
+    if(i>1){
+      lastRow=thisRow
+      thisRow=rep(list(),W)
+    }
+
     for(j in 1:W){
       if (i==1){ #first row (special case treated separately)
         if (j<x$w[1]){
           combinations[1,j]=0
+          thisRow[[j]]=c()
         }else{
           combinations[1,j]=x$v[1]
+          thisRow[[j]]=c(i)
         }
       }else{ #not the first row
         if (j<x$w[i]){
           combinations[i,j] = combinations[i-1,j]
+          thisRow[[j]]=lastRow[[j]]
         }else{
           diff = j - x$w[i]
           if (diff==0){
-            combinations[i,j] = max(combinations[i-1,j],x$v[i])
+
+            if (combinations[i-1,j]>x$v[i]){
+              combinations[i,j]=combinations[i-1,j]
+              thisRow[[j]]=lastRow[[j]]
+            }else{
+              combinations[i,j] = x$v[i]
+              thisRow[[j]]=c(i)
+            }
+            #combinations[i,j] = max(combinations[i-1,j],x$v[i])
           }else{
-            combinations[i,j] = max(combinations[i-1,j],x$v[i]+combinations[i-1,diff])
+
+            if (combinations[i-1,j]>(x$v[i]+combinations[i-1,diff])){
+              combinations[i,j]=combinations[i-1,j]
+              thisRow[[j]]=lastRow[[j]]
+            }else{
+              combinations[i,j] = x$v[i]+combinations[i-1,diff]
+              thisRow[[j]]=c(i,lastRow[[diff]])
+            }
+
+            #combinations[i,j] = max(combinations[i-1,j],x$v[i]+combinations[i-1,diff])
           }
         }
       }
@@ -33,6 +62,10 @@ knapsack_dynamic <- function(x, W){
 
   highest=max(combinations)
 
-  return(highest)
+  highest=combinations[length(x$v),W]
+  highestComb=thisRow[[W]]
+
+  returnList=list(value=highest,elements=x$origIndex[highestComb])
+  return(returnList)
 
 }
