8c68dd71b9186ed9f55f3fefdafdb1c18e6c543f
Rojan Karakaya <rojankarakaya@gmail.com>
2020-10-05 12:57:10 +0200

    Added better stopifnot-conditions, made sure to return solution element set as well as value

--- R/greedy_knapsack.r
+++ R/greedy_knapsack.r
@@ -1,9 +1,14 @@
 greedy_knapsack <- function(x, W){
-  stopifnot((length(x$v)==length(x$w)))
+  stopifnot(inherits(x,"data.frame"))
+  stopifnot(W>0)
 
   valueRatio = x$v/x$w
+  x$origIndex = 1:nrow(x)
 
   x=x[order(valueRatio, decreasing=TRUE),]
+
+  combination1=c()
+  combination=c()
 
   vikt1 = 0
   vikt = 0
@@ -15,13 +20,17 @@ greedy_knapsack <- function(x, W){
   while(vikt1<=W){
     vikt=vikt1
     varde=varde1
+    combination=combination1
 
     vikt1=vikt1+x$w[i]
     varde1=varde1+x$v[i]
+    combination1=c(combination1,i)
     i=i+1
   }
 
+  returnList=list(value=varde,elements=x$origIndex[combination])
 
-  return(varde)
+
+  return(returnList)
 
 }
--- R/knapsack_brute_force.r
+++ R/knapsack_brute_force.r
@@ -1,17 +1,21 @@
 knapsack_brute_force <- function(x, W){
-  stopifnot((length(x$v)==length(x$w)))
+  stopifnot(inherits(x,"data.frame"))
+  stopifnot(W>0)
 
   highestValue = 0
+  highestValueComb = c()
 
   for(i in 0:(2^(length(x$v))-1)){
     combination = which(as.logical(intToBits(i)))
 
-    if (sum(x$w[combination])<=W){
-      highestValue=max(c(sum(x$v[combination]), highestValue))
+    if ((sum(x$w[combination])<=W) & ((sum(x$v[combination])>highestValue))){
+      highestValue=sum(x$v[combination])
+      highestValueComb = combination
     }
 
   }
+  returnList=list(value=highestValue,elements=highestValueComb)
 
-  return(highestValue)
+  return(returnList)
 
 }
